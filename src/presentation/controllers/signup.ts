507c183ce25439c957e1dab6d72cae76d29630bd
Angeloqg <angeloqg@hotmail.com>
2021-03-16 06:43:06 -0300

    refactor: remove desnecessary tests

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,6 @@ import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from './../protocolos/http'
 export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
     const requiredFiels = ['name', 'email', 'password', 'passwordConfirmation']
 
     for (const field of requiredFiels) {
