4a506930ef9fcfc33d8caea84bc5f165b3cc7e88
Angeloqg <angeloqg@hotmail.com>
2021-03-18 07:07:55 -0300

    test: ensure SignUpController returns 500 if AddAccount throws

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -199,4 +199,26 @@ describe('SignUpController', () => {
       password: 'any_password'
     })
   })
+
+  test('Should return 500 if invalid AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut()
+
+    /* Utiliza o jest para alterar o emailValidatorStub, alterar a implementação
+       modificando oretorno */
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@email.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new ServerError())
+  })
 })
