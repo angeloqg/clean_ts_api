cce17508d144198f0e1da35f7a6981da81700553
Angeloqg <angeloqg@hotmail.com>
2021-03-31 07:04:58 -0300

    feat: ensure request return json as default content type

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,8 +1,10 @@
+import { contentType } from './../middlewares/content-type'
 import { Express } from 'express'
 import { bodyParser } from './../middlewares/body-parser'
 import { cors } from './../middlewares/cors'
 
 export default (app: Express): void => {
-  app.use(bodyParser)
   app.use(cors)
+  app.use(bodyParser)
+  app.use(contentType)
 }
