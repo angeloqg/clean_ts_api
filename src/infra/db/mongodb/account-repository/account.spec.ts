cb157b07923b773246139b993edefe00dd5302d9
Angeloqg <angeloqg@hotmail.com>
2021-04-03 21:27:54 -0300

    test: ensure mogodb reconnect if client is down

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,7 +11,7 @@ describe('Account Mongo Repository', () => {
   })
 
   beforeAll(async () => {
-    const accountCollection = MongoHelper.getCollection('accounts')
+    const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,18 @@
+import { MongoHelper as sut } from './mongo-helper'
+describe('Mongo Helper', () => {
+  beforeAll(async () => {
+    await sut.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await sut.disconnect()
+  })
+
+  test('Should reconnect if mongodb is down', async () => {
+    let accoutCollection = await sut.getCollection('accounts')
+    expect(accoutCollection).toBeTruthy()
+    await sut.disconnect()
+    accoutCollection = await sut.getCollection('accounts')
+    expect(accoutCollection).toBeTruthy()
+  })
+})
